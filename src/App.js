6367d7c4d75e1cb4b35a3f943f095ffe25b9312e
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2023-07-07 17:51:46 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import './App.css';
+import "./App.css";
 
-import { NavBar } from './components/NavBar/NavBar';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { MainPage } from './components/MainPage/MainPage';
-import  AuthPage  from './components/AuthPage/AuthPage';
-import SearchPage from './components/SearchPage/SearchPage';
-import { ResultPage } from './components/ResultPage/ResultPage';
-import Footer from './components/Footer/Footer';
+import { NavBar } from "./components/NavBar/NavBar";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { MainPage } from "./components/MainPage/MainPage";
+import AuthPage from "./components/AuthPage/AuthPage";
+import SearchPage from "./components/SearchPage/SearchPage";
+import { ResultPage } from "./components/ResultPage/ResultPage";
+import Footer from "./components/Footer/Footer";
+import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes";
 
 function App() {
+    return (
+        <div className="App">
+            <NavBar />
 
-  return (
-    <div className="App">
+            <Routes>
+                <Route element={<ProtectedRoutes />}>
+                    <Route path="/search/" element={<SearchPage />} />
+                    <Route path="/result/" element={<ResultPage />} />
+                </Route>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/login" element={<AuthPage />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
 
-      <NavBar />
-      
-      <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='login' element={<AuthPage />} />
-        <Route path='search' element={<SearchPage />} />
-        <Route path='result' element={<ResultPage />} />
-        <Route path='*' element={<Navigate to='/' />}/>
-      </Routes>
-
-      <Footer />
-    </div>
-  );
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
